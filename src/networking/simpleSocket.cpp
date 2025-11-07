90a9405ddb8e36778da6b4b2cd1749af3cb1abe0
Elliot <eteaisme@gmail.com>
2025-11-07 17:36:56 -0500

    Added getter functions for address, socket & connection

--- src/networking/simpleSocket.cpp
+++ src/networking/simpleSocket.cpp
@@ -11,12 +11,32 @@ HDE::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port,
     address.sin_addr.s_addr = htonl(interface);
 
     sock = socket(domain, service, protocol);
+
+    test_connection(sock); 
+    test_connection(connection); 
+
     connection = connect_to_network(sock, address); 
      
 }
+
+
 void HDE::SimpleSocket::test_connection(int item_to_test){
     if( item_to_test < 0 ){
         perror("Failed to connect");
         exit(EXIT_FAILURE);
     }
+}
+
+struct sockaddr_in HDE::SimpleSocket::get_address(){
+    return address; 
+}
+
+
+int HDE::SimpleSocket::get_sock(){
+    return sock; 
+}
+
+
+int HDE::SimpleSocket::get_connection(){
+    return connection; 
 }
--- src/networking/simpleSocket.h
+++ src/networking/simpleSocket.h
@@ -4,18 +4,22 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include<iostream>
+
 namespace HDE {
     class SimpleSocket{
+
         private:
             struct sockaddr_in address;
             int sock {}; 
             int connection {}; 
+
         public:
             SimpleSocket(int domain, int service, int protocall, int port, u_long interface);
-
             virtual int connect_to_network(int socket, struct sockaddr_in address) = 0;
-
             void test_connection(int);
+            struct sockaddr_in get_address(); 
+            int get_sock();
+            int get_connection();
     };
 }
 #endif
