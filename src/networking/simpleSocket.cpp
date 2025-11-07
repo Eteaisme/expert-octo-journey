2a672fe82aa9966ac7500473eaec9b3f33807ba6
Elliot <eteaisme@gmail.com>
2025-11-07 17:31:30 -0500

    Added test connection function

--- src/networking/simpleSocket.cpp
+++ src/networking/simpleSocket.cpp
@@ -1,14 +1,22 @@
 #include "simpleSocket.h" 
+#include <cstdio>
+#include <cstdlib>
 #include <netinet/in.h>
 #include <sys/socket.h>
 
 HDE::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface){
 
-    //Define address structure
     address.sin_family = domain; 
     address.sin_port = htons(port); 
     address.sin_addr.s_addr = htonl(interface);
 
-    //Establish connection
-    connection = socket(domain, service, protocol);
+    sock = socket(domain, service, protocol);
+    connection = connect_to_network(sock, address); 
+     
+}
+void HDE::SimpleSocket::test_connection(int item_to_test){
+    if( item_to_test < 0 ){
+        perror("Failed to connect");
+        exit(EXIT_FAILURE);
+    }
 }
